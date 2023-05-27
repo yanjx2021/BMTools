b14ce6c79baa2280c76aa7e48faa8cedfd54b683
yanjx21 <yjx2005@163.com>
2023-05-27 16:54:44 +0800

    update

--- bmtools/tools/gradio_tools/__init__.py
+++ bmtools/tools/gradio_tools/__init__.py
@@ -1,32 +1,6 @@
 from ..registry import register
 
-@register("stable_diffusion")
-def stable_diffusion():
-    from .stable_diffusion import build_tool
+@register("gradio_tools")
+def gradio():
+    from .api import build_tool
     return build_tool
-    
-@register("bark")
-def bark():
-    from .bark import build_tool
-    return build_tool
-
-@register("prompt_generator")
-def prompt_generator():
-    from .prompt_generator import build_tool
-    return build_tool
-
-@register("text_to_video")
-def text_to_video():
-    from .text_to_video import build_tool
-    return build_tool
-
-@register("image_captioning")
-def text_to_video():
-    from .image_captioning import build_tool
-    return build_tool
-
-@register("document_qa")
-def text_to_video():
-    from .document_qa import build_tool
-    return build_tool
-    
